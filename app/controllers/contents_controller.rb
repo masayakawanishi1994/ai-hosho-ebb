23db05cb6dddf86adcc0951517e9a28fbd924278
masayakawanishi1994 <seremade1.2.3@gmail.com>
2021-03-08 23:59:40 +0900

    test2

--- app/controllers/contents_controller.rb
+++ app/controllers/contents_controller.rb
@@ -1,34 +1,33 @@
 class ContentsController < ApplicationController
 # カテゴリーに応じたデータの保存の実装（⇨Formも）
-
   def new
     @content = Content.new
   end
 
   def index
     @banks = Bank.all
-    @categories = Category.all
+    @categories = Category.select(:subject)
     @content =Content.new
     @bank = Bank.find(params[:bank_id])
-    @category = @bank.categories.find(params[:category_id])
-    # @contents = @category.contents
+    @contents = @bank.contents
+        # @category = Category.find_by(subject: params[:subject])
+    # @category = @bank.categories.flat_map(&:categories)
   end
 
   def create
     @bank = Bank.find(params[:bank_id])
-    @category = @bank.categories
-    @content = @category.contents.new(content_params)
+    @category = Category.find_by(params[:category_id])
+    @content = @bank.contents.new(content_params)
     if @content.save
       redirect_to bank_contents_path(@bank)
     else
       @contents = @bank.contents.includes(:category)
-      render :index
+      render :index,notice: "登録できていません。"
     end
   end
-  
 
   private
   def content_params
-    params.require(:content).permit(:detail).merge(user_id: current_user.id)
+    params.require(:content).permit(:detail).merge(category_id: category.id)
   end
 end
